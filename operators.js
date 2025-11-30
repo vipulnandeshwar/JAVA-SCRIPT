9cbf0938897db2ce221c335dbf480dba6e655a44
vipulnandeshwar <nandeshwarvipul3@gmail.com>
2025-11-30 22:36:27 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,76 @@
+// Arithmitic Operators
+let a=10,b=20;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(b++);
+console.log(b);
+console.log(++a);
+console.log(a);
+console.log(b--);
+console.log(b);
+console.log(--a);
+console.log(a);
+console.log(2**4);
+console.log(3**3);
+
+//Assignment Operator
+let q=10,w=20;
+q=q+10;
+console.log(q);
+
+w+=5;
+console.log(w);
+
+w-=2;
+console.log(w);
+
+q*=5;
+console.log(q);
+
+q/=10;
+console.log(q);
+
+q%=5;
+console.log(q);
+
+//Relational Operator
+let s=80, d=85;
+console.log(s>d);
+
+console.log(s<d);
+
+console.log(s<=d);
+
+console.log(s>=d);
+
+console.log(5>=5);
+
+console.log(5<=5);
+
+console.log(5=="5");
+
+console.log(5=="5a");
+
+console.log(5!="5");
+
+console.log(10==="10");
+
+console.log(10!=="10");
+
+//Logical operator
+console.log((10>20)&&(20<10));
+console.log((10<20)&&(20<10));
+console.log((10<20)&&(20>10));
+console.log((10>20)&&(20>10));
+
+console.log((10>20)||(20<10));
+console.log((10<20)||(20<10));
+console.log((10<20)||(20>10));
+console.log((10>20)||(20>10));
+
+let f=true
+console.log(f);
+console.log(!f);
