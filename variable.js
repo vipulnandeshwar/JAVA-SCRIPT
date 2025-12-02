9c9744ce631c0033cbd11482e4eb87f762f0f58b
vipulnandeshwar <nandeshwarvipul3@gmail.com>
2025-12-02 12:30:43 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,61 @@
+var a=10
+console.log(a);
+let a1=20
+console.log(a1);
+const a2=30
+console.log(a2);
+console.log(window);
+
+//Hoisting
+
+console.log(x);
+var x=50
+console.log(x);
+
+// console.log(x1); //re
+let x1=80
+console.log(x1);
+
+// console.log(x2); //re
+const x2=85
+console.log(x2);
+
+//re-dec
+
+var x=20
+console.log(x);
+
+// let x1=25//se
+// console.log(x1);
+
+// const x2=88//se
+// console.log(x2);
+
+//re-init
+
+x=22
+console.log(x);
+x1=45
+console.log(x1);
+
+// x2=35
+// console.log(x2);
+
+//Declare
+var q
+console.log(q);
+
+let q1
+console.log(q1);
+
+// const q2//se
+// console.log(q2);
+
+//init
+
+q=12
+console.log(q);
+q1=13
+console.log(q1);
+
+
