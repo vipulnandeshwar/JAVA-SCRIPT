1ebcf60a95b2ab790f8d21308679ce34b8bb288b
vipulnandeshwar <nandeshwarvipul3@gmail.com>
2025-12-03 11:51:43 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,53 @@
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+
+
+for(let a=1;a<=5;a++){
+    console.log("Hello");
+    console.log("\n");
+}
+
+let x="";
+for(let a=1;a<=5;a++){
+    console.log("Hello");
+    x+="hrllo \n"
+}
+
+console.log(x);
+for(let a=1;a<=5;a++){
+    console.log("Hello"+a);
+}
+document.write("<br>")
+for(let i=1;i<=5;i++){
+    for(let j=1;j<=5;j++){
+        document.writeln("*")
+    }
+    document.writeln("<br>")
+}
+
+let str1=""
+for(let i=1;i<=5;i++){
+    for(let j=1;j<=5;j++){
+        str1+="* "
+    }
+    str1+="\n"
+}
+console.log(str1);
+
+
+let k=2
+while (k<=5) {
+    console.log("hello");
+    k++
+}
+
+let w=1
+do {
+    console.log("hi do while");
+    w++;
+    
+} 
+while (w<=5)
