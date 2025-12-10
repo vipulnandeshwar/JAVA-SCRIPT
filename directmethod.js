849df6be5f78815e59c1f440d2b2369180e3c1c7
vipulnandeshwar <nandeshwarvipul3@gmail.com>
2025-12-10 10:02:31 +0530

    Add files via upload

---
+++ directmethod.js
@@ -0,0 +1,26 @@
+document.write("hello write method")
+document.write("hi")
+document.write("<br>")
+document.writeln("hello")
+document.writeln("hi")
+
+let a=document.body
+console.log(a);
+console.log(document.head);
+console.log(document.title);
+
+let f=document.forms
+console.log(f);
+console.log(f[1]);
+
+let i=document.images
+console.log(i);
+console.log(i[0]);
+
+let anchor=document.links
+console.log(anchor);
+console.log(anchor[2]);
+
+console.log(document.URL);
+console.log(document.baseURI);
+
---
+++ windows.js
@@ -0,0 +1,31 @@
+let a=window.prompt("enter your name")
+console.log(a);
+console.log(typeof a);
+
+let x=Number(prompt("enter your first number"))
+console.log(x);
+console.log(typeof x);
+
+let y=Number(prompt("enter your second number"))
+console.log(y);
+console.log(typeof y);
+console.log(x+y);
+
+let q=window.alert("this is your alert msg")
+console.log(q);
+
+alert("this is your second alert")
+
+let s=window.confirm("this is your conformation msg")
+console.log(s);
+
+console.warn("this is your warning msg");
+console.error("this is your error msg");
+
+let d=Number(prompt("Enter value"))
+let b=Number(prompt("Enter 1st anumber"))
+let e=Number(prompt("Enter 2st anumber"))
+
+for(let i=b;i<=e;i++){
+    document.writeln()
+}
